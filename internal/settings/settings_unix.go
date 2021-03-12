236393e0244c1da1abc1b84c34be60faacc1cfa5
Simon Edwards <simon@simonzone.com>
2021-03-12 22:37:10 +0100

    Fix up some copyright headers

--- internal/settings/settings_unix.go
+++ internal/settings/settings_unix.go
@@ -1,4 +1,9 @@
 // +build !windows
+/*
+ * Copyright 2021 Simon Edwards <simon@simonzone.com>
+ *
+ * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
+ */
 package settings
 
 import (
